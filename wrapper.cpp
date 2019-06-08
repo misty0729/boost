cb11d5d3e2701e9ffa6dbfb91057aff2177b2a62
qianzgit <qianz_need@163.com>
2019-06-08 13:23:35 +0800

    add

--- wrapper.cpp
+++ wrapper.cpp
@@ -1,8 +1,8 @@
 extern "C" #include "detex.h"
-#include <boost/python.hpp>
+//#include <boost/python.hpp>
 #include <iostream>
 
-using namespace boost::python;
+// using namespace boost::python;
 
  
 void greet()
@@ -10,7 +10,7 @@ void greet()
     std::cout << "hello cute  bbh!" << std::endl;
 }
  
-BOOST_PYTHON_MODULE(mygreet)
-{
-    def("greet", greet);
-}
+// BOOST_PYTHON_MODULE(mygreet)
+// {
+//     def("greet", greet);
+// }
